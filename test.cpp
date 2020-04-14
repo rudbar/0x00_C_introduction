391219be502da95a83f6ddc97525e60ea486d918
Ian Kachan <kachan.yanf@gmail.com>
2020-04-14 17:34:06 +0300

    Added the examples of variables of different types and also added a couple of screenshots showing the code, the disassembly language, the registers and the watch windows

--- test.cpp
+++ test.cpp
@@ -3,7 +3,7 @@
 // Определение функции
 void foo(void)  
 {
-	const char *Foo = "This is the first thing we printed out.\n";
+	const char *Foo = "This is the first thing we printed out.\r\n";
 	OutputDebugStringA(
 		"Линия 0\\Линия 1\n");
 }
@@ -15,6 +15,20 @@ int WinMain(
 	int       nShowCmd
 )
 {
+	char SmallS; // 8 bits - 256 different values [-128,127]
+	char unsigned SmallU; // 8 bits unsigned - 256 different values [0, 255]
+
+	short MediumS; // 16 bits - 65536 different values
+	short unsigned MediumU;
+
+	int LargeS; // 32 bits - 4 billion
+	int unsigned LargeU;
+
+	char unsigned Test;
+
+	Test = 255;
+	Test = Test + 1;
+	
 	foo();
 }
 
